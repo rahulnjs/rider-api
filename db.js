96f0418b20e25d2a83b32024bcaac896542898f5
rahulnjs <rahulsharmaa@hotmail.com>
2025-06-17 20:09:45 +0530

    okay

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import { MongoClient } from 'mongodb';
 
 async function connect() {
-    const url = `mongodb+srv://root:${process.env.DB_PWD}@riders.hd4vtve.mongodb.net/admin?authSource=admin&replicaSet=atlas-7ub5o0-shard-0&w=majority&readPreference=primary&appname=riders&retryWrites=true&ssl=true`;
+    const url = `mongodb+srv://root:${process.env.RIDERS_API_DB_PWD}@riders.hd4vtve.mongodb.net/admin?authSource=admin&replicaSet=atlas-7ub5o0-shard-0&w=majority&readPreference=primary&appname=riders&retryWrites=true&ssl=true`;
     const client = new MongoClient(url);
     await client.connect();
     console.log('Connected successfully to server');
-    const db = client.db(process.env.DATABASE);
+    const db = client.db(process.env.RIDERS_API_DATABASE);
     return db;
 }
 
