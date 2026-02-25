dc209ce79616accae9b292166355b10871e63ae1
rahulnjs <rahulsharmaa@hotmail.com>
2026-02-25 18:04:03 +0530

    some

--- api/expense.js
+++ api/expense.js
@@ -1,18 +1,20 @@
 async function routes(fastify, { _db: db }) {
 
-    fastify.get('/expense/data', async function (req, res) {
+    fastify.get('/:db/data', async function (req, res) {
         try {
-            const all = await db.collection('expense').find({}).toArray();
+            const { db } = req.params;
+            const all = await db.collection(db).find({}).toArray();
             return res.send(all || []);
         } catch (err) {
             res.code(500).send({ error: 'Failed to fetch data', details: err.message });
         }
     });
 
-    fastify.put('/expense/data', async function (req, res) {
+    fastify.put('/:db/data', async function (req, res) {
         try {
+            const { db } = req.params;
             const { cycle, budget } = req.body;
-            const all = await db.collection('expense').updateOne({ cycle }, { $set: { budget } }, { upsert: true });
+            const all = await db.collection(db).updateOne({ cycle }, { $set: { budget } }, { upsert: true });
             return res.send(all || []);
         } catch (err) {
             res.code(500).send({ error: 'Failed to fetch data', details: err.message });
